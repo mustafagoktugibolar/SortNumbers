28cfc65374c0d16de507a38af43b7bb52e588165
mustafagoktugibolar <mustafagoktugibolar@gmail.com>
2023-02-16 16:42:57 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -16,19 +16,24 @@ public class Main {
         if (a > b && a > c) {
             if (b > c) {
                 System.out.println("a > b > c");
-            } else {
+            } 
+            else {
                 System.out.println("a > c > b");
             }
-        } else if (b > a && b > c) {
+        }
+        else if (b > a && b > c) {
             if (a > c) {
                 System.out.println("b > a > c");
-            } else {
+            } 
+            else {
                 System.out.println("b > c > a");
             }
-        } else {
+        } 
+        else {
             if (a > b) {
                 System.out.println("c > a > b");
-            } else {
+            } 
+            else {
                 System.out.println("c > b > a");
             }
         }
